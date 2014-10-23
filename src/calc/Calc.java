5a3d37376627c5f24c1261d6cc1993df68914f7c
Sala1 <Sala1@Sala1-PC12>
2014-10-23 09:54:11 -0500

    inicio de proyecto

--- src/calc/Calc.java
+++ src/calc/Calc.java
@@ -1,10 +1,22 @@
 
 package calc;
 public class Calc {
-    public static void main(String[] args) {
-    //los achivos en color verde son colores nuevos
-        
-        
+    double a;
+    double b;
+    static double suma(double a, double b)
+    {
+   
+         return a + b;
     }
-    
+    static double resta(double a, double b)
+    {
+        return a - b;
+    }
+            
+            public static void main(String[] args) {
+        
+    //los achivos en color verde son colores nuevos
+      
 }
+     
+    }
